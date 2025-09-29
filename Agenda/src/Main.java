42eaf73fe55363682f8dfb98ce7ba6b56a3d0781
HugoGonzalezOrtega2007 <gonzalezhugoortega@digitechfp.com>
2025-09-29 08:58:11 +0200

    ACTUALIZACION

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("- Añadir contacto");
+        System.out.println("- Mostrar contactos");
+        System.out.println("- Buscar contacto");
+        System.out.println("- Salir");
+
+        System.out.print("Escriba la opcion que quiere seleccionar: ");
+
+        String option = scanner.nextLine();
+        System.out.println("Has escogido: " + option);
+
+        scanner.close();
+    }
 }
