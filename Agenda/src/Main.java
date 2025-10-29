8df785de9925dec921302930024c38693ea695ab
HugoGonzalezOrtega2007 <gonzalezhugoortega@digitechfp.com>
2025-10-29 12:05:47 +0100

    ACTUALIZAR

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -4,15 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Seleccione un menú: ");
+
         System.out.println("- Añadir contacto");
         System.out.println("- Mostrar contactos");
         System.out.println("- Buscar contacto");
         System.out.println("- Salir");
 
-        System.out.print("Escriba la opcion que quiere seleccionar: ");
-
-        String option = scanner.nextLine();
-        System.out.println("Has escogido: " + option);
+        String opcion = scanner.nextLine();
+        if (opcion.equals("Añadir contacto")) {
+            System.out.println("Has escogido: " + opcion);
+        } else if (opcion.equals("Mostrar contactos")) {
+            System.out.println("Has escogido: " + opcion);
+        } else if (opcion.equals("Buscar contacto")) {
+            System.out.println("Has escogido: " + opcion);
+        } else {
+            System.out.println("Has escogido: " + opcion);
+        }
 
         scanner.close();
     }
