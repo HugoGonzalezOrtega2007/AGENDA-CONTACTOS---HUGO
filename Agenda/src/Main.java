0d8b0bcccc6ed8f85a7c30c91998b91e4cb83740
HugoGonzalezOrtega2007 <gonzalezhugoortega@digitechfp.com>
2025-11-28 11:36:47 +0100

    cambiosfinales

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -11,38 +11,57 @@ public class Main {
         boolean salir = false;
         while (!salir) {
 
-            System.out.println("\nSeleccione un menú:");
-            System.out.println("- Añadir contacto");
-            System.out.println("- Mostrar contactos");
-            System.out.println("- Buscar contacto");
-            System.out.println("- Salir");
+            System.out.println("== Seleccione un menú ==");
+            System.out.println("1 - Añadir contacto");
+            System.out.println("2 - Mostrar contactos");
+            System.out.println("3 - Modificar contacto");
+            System.out.println("4 - Buscar contacto");
+            System.out.println("5 - Salir");
 
             System.out.print("Opción: ");
-            String opcion = scanner.nextLine();
+            int opcion = scanner.nextInt();
 
-            if (opcion.equals("Añadir contacto")) {
+            if (opcion==1) {
                 System.out.println("Has escogido añadir contacto");
-                System.out.println("Añade el nombre de tu contacto: ");
-                String nombre = scanner.nextLine();
+                //NOMBRE
+                System.out.print("Añade el nombre de tu contacto: ");
+                String nombre = scanner.next();
                 listanombre[indice] = nombre;
-                indice++;
-                System.out.println("Añade el numero de tu contacto: ");
+                //NUMERO
+                System.out.print("Añade el numero de tu contacto: ");
                 int numero = scanner.nextInt();
                 listanumero[indice] = numero;
                 indice++;
             }
-            else if (opcion.equals("Mostrar contactos")) {
+            else if (opcion==2) {
                 System.out.println("Has escogido mostrar contactos");
                 for (int i = 0; i < listanombre.length; i++) {
                     if (listanombre[i] != null) {
-                        System.out.println((i + 1) + "- " + listanombre[i] + " (" + listanumero[i] + ")");
+                        System.out.println((i + 1) + " - " + listanombre[i] + " (" + listanumero[i] + ")");
                     }
                 }
             }
-            else if (opcion.equals("Buscar contacto")) {
-                System.out.println("Has escogido: " + opcion);
+            else if (opcion==3) {
+                System.out.println("Has escogido modificar contacto");
+                for (int i = 0; i < listanombre.length; i++) {
+                    if (listanombre[i] != null) {
+                        System.out.println((i + 1) + " - " + listanombre[i] + " (" + listanumero[i] + ")");
+                    }
+                }
+                System.out.print("Que contacto quieres modificar? ");
+                int modificar = scanner.nextInt();
+                    System.out.println("Vale, modificando " + listanombre[modificar - 1]);
+                    System.out.print("Añade el nuevo nombre de tu contacto: ");
+                    String nuevoNombre = scanner.next();
+                    listanombre[modificar - 1] = nuevoNombre;
+                    System.out.print("Añade el nuevo numero de tu contacto: ");
+                    int nuevoNumero = scanner.nextInt();
+                    listanumero[modificar - 1] = nuevoNumero;
             }
-            else if (opcion.equals("Salir")) {
+            else if (opcion==4) {
+                System.out.println("Has escogido buscar contactos");
+            }
+            else if (opcion==5) {
                 System.out.println("Saliendo del programa...");
                 salir = true;
             }
