2506830850135772f007ec91ad8d1bd07acdbd1b
HugoGonzalezOrtega2007 <gonzalezhugoortega@digitechfp.com>
2026-02-17 14:27:42 +0100

    objetos

---
+++ Agenda/src/Contacto.java
@@ -0,0 +1,29 @@
+public class Contacto {
+
+    private String nombre;
+    private int numero;
+
+    public Contacto(String nombre, int numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + numero + ")";
+    }
+}
--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,102 +1,81 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        boolean salir = false;
-        String[] listaNombre = new String[100];
-        int[] listaNumero = new int[100];
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Contacto[] listaContactos = new Contacto[100];
         int indice = 0;
-        int opcion = 0;
-        String nombre = "";
-        int numero = 0;
-        int modificar = 0;
-        String nuevoNombre = "";
-        int nuevoNumero = 0;
 
-        print(salir, listaNombre, listaNumero, indice, opcion, nombre, numero, modificar, nuevoNombre, nuevoNumero);
+        print(listaContactos, indice);
     }
-    public static void añadirContacto (String[] listaNombre, int[] listaNumero, int indice, String nombre, int numero) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Has escogido añadir contacto.");
-
-        //NOMBRE
-        System.out.print("Añade el nombre de tu contacto: ");
-        nombre = scanner.next();
-        listaNombre[indice] = nombre;
-
-        //NUMERO
-        System.out.print("Añade el numero de tu contacto: ");
-        numero = scanner.nextInt();
-        listaNumero[indice] = numero;
-        indice++;
-
-        System.out.println("");
-    }
-    public static void mostrarContactos (String[] listaNombre, int[] listaNumero) {
-        System.out.println("Has escogido mostrar contactos.");
-        for (int i = 0; i < listaNombre.length; i++) {
-            if (listaNombre[i] != null) {
-                System.out.println((i + 1) + " - " + listaNombre[i] + " (" + listaNumero[i] + ")");
-            }
-        }
-        System.out.println("");
-    }
-    public static void modificarContacto (String[] listaNombre, int[] listaNumero, int indice, String nombre, int numero, int modificar, String nuevoNombre, int nuevoNumero) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Has escogido modificar contacto.");
-        for (int i = 0; i < listaNombre.length; i++) {
-            if (listaNombre[i] != null) {
-                System.out.println((i + 1) + " - " + listaNombre[i] + " (" + listaNumero[i] + ")");
-            }
-        }
-
-        System.out.print("Que contacto quieres modificar? ");
-        modificar = scanner.nextInt();
-        System.out.println("Vale, modificando " + listaNombre[modificar - 1] + ".");
-
-        System.out.print("Añade el nuevo nombre de tu contacto: ");
-        nuevoNombre = scanner.next();
-        listaNombre[modificar - 1] = nuevoNombre;
-
-        System.out.print("Añade el nuevo numero de tu contacto: ");
-        nuevoNumero = scanner.nextInt();
-        listaNumero[modificar - 1] = nuevoNumero;
-        System.out.println("");
-    }
-    public static void salir (boolean salir, int opcion) {
-        System.out.println("Saliendo del programa...");
-    }
-
-    static void print (boolean salir, String[] listaNombre, int [] listaNumero, int indice, int opcion, String nombre, int numero, int modificar, String nuevoNombre, int nuevoNumero) {
-        salir = false;
+    public static void print (Contacto[] listaContactos, int indice) {
+        boolean salir = false;
         while (!salir) {
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("== MENÚ ==");
+            System.out.println("==== MENÚ ====");
             System.out.println("1 - Añadir contacto | 2 - Mostrar contactos | 3 - Modificar contacto | 4 - Salir");
             System.out.print("Introduzca el número de la opción a la que quiere acceder: ");
-            opcion = scanner.nextInt();
-            System.out.println("");
+            int opcion = scanner.nextInt();
+            System.out.println();
 
-            if (opcion==1) {
-                añadirContacto(listaNombre, listaNumero, indice, nombre, numero);
+            switch (opcion) {
+                case 1:
+                    indice = añadirContacto(listaContactos, indice);
+                    break;
+                case 2:
+                    mostrarContactos(listaContactos);
+                    break;
+                case 3:
+                    modificarContacto(listaContactos);
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa...");
+                    salir = true;
+                    break;
             }
+        }
+    }
+    public static int añadirContacto(Contacto[] listaContactos, int indice) {
+        System.out.print("Añade el nombre del contacto: ");
+        String nombre = scanner.next();
 
-            else if (opcion==2) {
-                mostrarContactos(listaNombre, listaNumero);
-            }
+        System.out.print("Añade el número del contacto: ");
+        int numero = scanner.nextInt();
 
-            else if (opcion==3) {
-                modificarContacto(listaNombre, listaNumero, indice, nombre, numero, modificar, nuevoNombre, nuevoNumero);
-            }
+        listaContactos[indice] = new Contacto(nombre, numero);
 
-            else if (opcion==4) {
-                salir(salir, opcion);
-                salir = true;
+        System.out.println("Contacto añadido correctamente.\n");
+
+        return indice + 1;
+    }
+    public static void mostrarContactos(Contacto[] listaContactos) {
+        System.out.println("Lista de contactos:");
+        for (int i = 0; i < listaContactos.length; i++) {
+            if (listaContactos[i] != null) {
+                System.out.println((i + 1) + " - " + listaContactos[i]);
             }
+        }
+        System.out.println();
+    }
+    public static void modificarContacto(Contacto[] listaContactos) {
+        mostrarContactos(listaContactos);
+
+        System.out.print("¿Qué contacto quieres modificar? ");
+        int posicion = scanner.nextInt() - 1;
+
+        if (posicion >= 0 && posicion < listaContactos.length && listaContactos[posicion] != null) {
+            System.out.print("Añade el nuevo nombre del contacto: ");
+            String nuevoNombre = scanner.next();
+            listaContactos[posicion].setNombre(nuevoNombre);
+
+            System.out.print("Añade el nuevo número del contacto: ");
+            int nuevoNumero = scanner.nextInt();
+            listaContactos[posicion].setNumero(nuevoNumero);
+
+            System.out.println("Contacto modificado correctamente.\n");
+
+        } else {
+            System.out.println("No tienes ese número de contacto.\n");
         }
     }
 }
